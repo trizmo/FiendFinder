64cf746c6b5be9cbf7bdc0656cf6d89fff90b218
Tristan Perera <tristan.e.perera@gmail.com>
2018-12-05 15:47:28 -0800

    added server and data files

---
+++ fiends.js
@@ -0,0 +1,19 @@
+module.exports = [
+
+  {
+    name: "Lisa",
+    photo: "https://www.placecage.com/200/300",
+    scores: [3, 1, 5, 3, 2, 5, 1, 3, 3, 2]
+  },
+  {
+    name: "Elizabeth",
+    photo: "https://www.placecage.com/200/300",
+    scores: [3, 1, 4, 3, 2, 5, 4, 2, 3, 2]
+  },
+  {
+    name: "Dawn",
+    photo: "https://www.placecage.com/200/300",
+    scores: [2, 1, 2, 3, 2, 4, 1, 1, 3, 2]
+  }
+
+]
